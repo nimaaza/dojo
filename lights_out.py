fed086705299c7d007645788e77b23a37d91524b
Nima Azarbayjani <nimaza@gmail.com>
2022-05-13 16:46:03 +0200

    Print game messages according to board size

--- lights_out.py
+++ lights_out.py
@@ -32,8 +32,16 @@ def toggle_light(board, row, column):
     if column + 1 < 3:
         board[row][column + 1] = not board[row][column + 1]
 
-board = [[True, True, True], [True, True, True], [True, True, True]]
-prompt = "Which light would you like to toggle (row column)? "
+def acceptable_numbers(max):
+    s = ""
+    for i in range(max):
+        s += str(i + 1)
+        if i < max - 2:
+            s += ", "
+        elif i == max - 2:
+            s += ", and "
+    
+    return s
 
 board_size = 0
 
@@ -47,6 +55,9 @@ while(True):
 
 board_row = [True for i in range(board_size)]
 board = [board_row[:] for i in range(board_size)]
+
+input("Enter the row and column number of the light you want to toggle separated with space (hit enter to continue).")
+input(f"Enter a random character other than {acceptable_numbers(board_size)} to accept defeat (hit enter to start).")
 
 while(not game_over(board)):
     print_board(board)
